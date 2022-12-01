484f09aa52cc7423a7b855e5d15642af610a7695
Artem Filippow <a.filippov@atech.ru>
2022-12-01 22:00:56 +0900

    fixed to 2 digits

--- frontend/pages/specifications/_specId.vue
+++ frontend/pages/specifications/_specId.vue
@@ -78,8 +78,8 @@
         <td>
           <ConfigurationCount :item="config" :onChange="loadSpec"/>
         </td>
-        <td align="right">{{ config.price }}</td>
-        <td align="right">{{ config.priceTotal }}</td>
+        <td align="right">{{ config.price.toFixed() }}</td>
+        <td align="right">{{ config.priceTotal.toFixed() }}</td>
       </tr>
       <tr>
         <td colspan="5">
